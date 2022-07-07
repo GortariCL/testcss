8abad9dea4ebc455d8176c2aa8a33e13e47ab9b7
Marcelo Huenchupan <mhuenchupan@mobdev.cl>
2022-07-07 13:11:28 -0400

    intento con flex xD

--- src/App.js
+++ src/App.js
@@ -8,18 +8,15 @@ function App() {
       <nav>el nav </nav>
       <div className="container">
         <div className="container-1">
-          <div className="container-1__img">
-            <img src={Elipse} alt="pepe" />
+          <div className="container-1__1">
+            <img className="elipse-img" src={Elipse} alt="pepe" /> 
           </div>
-
-          <div className="container-1__text">
-            <h6>Invita a quien tu quieras</h6>
-            <h3>Crea tus eventos</h3>
-            <p>Organiza y diseña sadfojiasfdiojdsaf </p>
-          </div>
+          <div className="container-1__2"></div>
         </div>
 
-        <div className="container-2">2</div>
+        <div className="container-2">
+          
+        </div>
         <div className="container-3">3</div>
       </div>
     </div>
